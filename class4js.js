0817e7c6e8b4e39cee9500af233d11b8ac39f392
02prachi2002 <211288@juitsolan.in>
2023-08-23 14:59:14 +0530

    commit

--- class4js.js
+++ class4js.js
@@ -17,10 +17,21 @@ console.log('second');
 
 
 //Promises-koi bhi command ya function ya kuch bhi hum parallely execute karwana chahete hai background mai
+//promise has two phases -> resolve  --> reject
 //then we use promise
 //sync promise example ---  
 let merapromise  = new Promise(function(resolve , reject){
     console.log('i am inside promise');
-    resolve(2);
+    resolve(2);//by using resolve we use to mark th epromise -> fulfilled
 });
 console.log('pehle promise');
+
+//for reject
+let mypromise = new Promise (function (resolve,reject)
+{
+    setTimeout(function(){
+        console.log('i am inside promise');
+    },5000);
+    reject (new Error('this is an error when promise doesnt resolve'))
+});
+console.log('promise');
